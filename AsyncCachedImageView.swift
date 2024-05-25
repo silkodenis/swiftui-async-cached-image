e1280e0f40156bff57cf26fe77c12f062d0893c8
Denis Silko <silkodenis@gmail.com>
2024-05-25 14:28:55 +0300

    Initial commit

---
+++ AsyncCachedImageView.swift
@@ -0,0 +1,104 @@
+/*
+ * Copyright (c) [2024] [Denis Silko]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     https://github.com/silkodenis/swiftui-async-cached-image
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import SwiftUI
+import Combine
+
+/// A view that asynchronously loads and caches an image from a URL,
+/// displaying a placeholder while the image is being loaded.
+struct AsyncCachedImageView<ImageView: View, PlaceholderView: View>: View {
+    private let url: URL?
+    private let content: (Image) -> ImageView
+    private let placeholder: () -> PlaceholderView
+    @State private var image: UIImage? = nil
+    @State private var cancellable: AnyCancellable?
+
+    /// Initializes the AsyncCachedImageView with a URL, content view, and placeholder view.
+    ///
+    /// - Parameters:
+    ///   - url: The URL to load the image from.
+    ///   - content: A view builder that takes a loaded `Image` and returns the view to display.
+    ///   - placeholder: A view builder that provides the placeholder view to display while the image is loading.
+    init(url: URL?,
+         @ViewBuilder content: @escaping (Image) -> ImageView,
+         @ViewBuilder placeholder: @escaping () -> PlaceholderView) {
+        self.url = url
+        self.content = content
+        self.placeholder = placeholder
+    }
+
+    var body: some View {
+        VStack {
+            if let uiImage = image {
+                content(Image(uiImage: uiImage))
+            } else {
+                placeholder()
+                    .onAppear(perform: loadImage)
+                    .onDisappear(perform: cancelLoading)
+            }
+        }
+    }
+    
+    // MARK: - Private
+
+    /// Loads the image from the URL asynchronously, using a cached image if available.
+    private func loadImage() {
+        guard let url = url else { return }
+
+        if let cachedImage = loadCachedImage(for: url) {
+            self.image = cachedImage
+            return
+        }
+
+        cancellable = URLSession.shared.dataTaskPublisher(for: url)
+            .map { data, response in
+                self.cacheImage(data, response: response, for: url)
+                return UIImage(data: data)
+            }
+            .replaceError(with: nil)
+            .receive(on: DispatchQueue.main)
+            .assign(to: \.image, on: self)
+    }
+
+    /// Cancels the image loading task.
+    private func cancelLoading() {
+        cancellable?.cancel()
+    }
+
+    /// Loads the cached image for a given URL, if available.
+    ///
+    /// - Parameter url: The URL of the image to load from cache.
+    /// - Returns: The cached `UIImage`, or `nil` if no cached image is available.
+    private func loadCachedImage(for url: URL) -> UIImage? {
+        if let cachedResponse = URLCache.shared.cachedResponse(for: URLRequest(url: url)) {
+            return UIImage(data: cachedResponse.data)
+        }
+        return nil
+    }
+
+    /// Caches the image data for a given URL.
+    ///
+    /// - Parameters:
+    ///   - data: The image data to cache.
+    ///   - response: The URL response associated with the image data.
+    ///   - url: The URL of the image to cache.
+    private func cacheImage(_ data: Data, response: URLResponse, for url: URL) {
+        let cachedResponse = CachedURLResponse(response: response, data: data)
+        URLCache.shared.storeCachedResponse(cachedResponse, for: URLRequest(url: url))
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,64 @@
-# swiftui-async-cached-image
+[![License](https://img.shields.io/github/license/silkodenis/swiftui-async-cached-image.svg)](https://github.com/silkodenis/swiftui-async-cached-image/blob/main/LICENSE)
+
+# AsyncCachedImage
+
+AsyncCachedImage is a SwiftUI view that asynchronously loads and caches an image from a URL, displaying a placeholder while the image is being loaded. This implementation is compatible with iOS 13 and later, providing greater flexibility and control over image loading and caching.
+
+## Features
+
+- Asynchronous image loading from a URL
+- Image caching using `URLCache`
+- Customizable content and placeholder views
+- Compatible with iOS 13 and later
+
+## Requirements
+
+- **iOS 13**+
+- **Xcode 11**+
+- **Swift 5**+
+
+## Installation
+
+To use `AsyncCachedImage` in your project, simply copy the `AsyncCachedImage.swift` file into your project.
+
+## Usage
+
+Here's an example of how to use `AsyncCachedImage` in your SwiftUI view:
+
+```swift
+import SwiftUI
+
+struct ContentView: View {
+    let imageUrl = URL(string: "https://example.com/image.jpg")
+
+    var body: some View {
+        AsyncCachedImage(url: imageUrl) { image in
+            image
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+        } placeholder: {
+            Text("Loading...")
+        }
+    }
+}
+```
+## Comparison with AsyncImage
+
+AsyncImage is a built-in SwiftUI view introduced in iOS 15 that also allows asynchronous image loading from a URL. Here’s a comparison of AsyncCachedImage and AsyncImage:
+
+### AsyncCachedImage
+
+-    iOS Compatibility: Works with iOS 13+
+-    Caching: Explicitly manages image caching using URLCache
+-    Customizability: Allows custom content and placeholder views
+-    Control: Provides greater control over the image loading process
+
+### AsyncImage
+
+-    iOS Compatibility: Available in iOS 15+
+-    Caching: Automatically handles caching
+-    Customizability: Limited customization options compared to AsyncCachedImage
+-    Control: Easier to use for simple cases, but less control over the loading process
+
+## License
+This project is licensed under the [Apache License, Version 2.0](LICENSE).
